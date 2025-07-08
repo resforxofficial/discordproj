fc2f459aa4c7b7134c22238dc1df35a5a25f30e4
resforxofficial <paedeogim@gmail.com>
2025-07-08 22:38:48 +0900

    Update build.ts
    not including env

--- build.ts
+++ build.ts
@@ -1,5 +1,19 @@
-import { REST, Routes, SlashCommandBuilder } from 'discord.js';
-import dotenv from 'dotenv';
-import path from 'path';
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName("riot-lol")
+        .setDescription("hand-made opgg in discord (sorry for bad description)")
+        .addStringOption((opt) =>
+            opt.setName("gameName").setDescription("게임이름").setRequired(true)
+        )
+        .addStringOption((opt) =>
+            opt.setName("tagLine").setDescription("태그 (# 뒷부분)").setRequired(true)
+        )
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.API_KEY!)
